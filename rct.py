acfdcb4dfe2dcf35b8a4bf32311f2df165e6e460
Avery O <ave@ave.zone>
2017-05-07 15:17:58 +0300

    switch to http head requests, improving speed drastically

--- rct.py
+++ rct.py
@@ -1,5 +1,4 @@
-import urllib.request
-import urllib.error
+import http.client
 import traceback
 
 global count
@@ -14,21 +13,22 @@ def get_subreddit_list():
     except Exception:
         print(traceback.format_exc())
 
-while 1:
-	subslist = get_subreddit_list()
-	count = 0
-	for val in subslist:
-		count = count + 1
-		try:
-			req = urllib.request.Request(
-    			"https://www.reddit.com/r/"+val+".json", # Simplest way to check seems to be the JSON endpoint- the output is shorter than the actual subreddit's HTML, and as the only way to see if a sub is blocked is just checking for HTTP responses, this seems like an easy way to check is this.
-    			data=None, 
-    			headers={
-        			'User-Agent': 'python3:turkey_block_scanner_part2:v1.1 (by /u/ardaozkal)'
-    			}
-			)
-			output = urllib.request.urlopen(req)
-			print("Not blocked ("+str(count)+"): " + val)
-		except urllib.error.HTTPError as e:
-			if int(e.code) == 451:
-				print("Block found ("+str(count)+"): " + val)
+subslist = get_subreddit_list()
+count = 0
+for val in subslist:
+	count = count + 1
+	try:
+            conn = http.client.HTTPConnection("www.reddit.com")
+            conn.putrequest("HEAD", "/r/"+val+".json")
+            conn.putheader("User-Agent", "python3:turkey_block_scanner_part2:v1.1 (by /u/ardaozkal)")
+            conn.endheaders()
+            conn.send("")
+            response = conn.getresponse()
+            if str(response.status) == "451":
+                print(val + ": Blocked")
+            else:
+                print(val + ": Not blocked")
+	except Exception:
+		print(traceback.format_exc())
+
+print("Done.")
